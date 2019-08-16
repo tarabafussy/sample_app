08c6608f4d664cc351368ef059553d9b87ecbde8
tarabafussy <sasabinfussy@gmail.com>
2019-08-16 11:08:42 +0900

    [3-6]Start

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_063338) do
+ActiveRecord::Schema.define(version: 2019_08_16_020333) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
