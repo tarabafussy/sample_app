1a29fcd455b24f5d742f76c11e1ee8ac866f3731
tarabafussy <sasabinfussy@gmail.com>
2019-08-16 13:25:52 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1a2f9ef67b769d47314fdeed24d4a41c992a8a346f912fe99e4c9e0f4607c248c134d80f70c796d5a940fa2e91997b95dcd7239b6bdbe3753037aeb2921c9eb9'
